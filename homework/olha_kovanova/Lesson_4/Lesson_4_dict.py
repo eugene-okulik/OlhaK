809360df6b8e50e62d59cb08f7b80be425f6b1fd
OlhaKovanova <kovanova.olha@gmail.com>
2024-07-01 13:06:11 +0200

    I deleted some extra space in order to resolve the PEP8 errors, I hope it helps, otherwise I;m done :))))

--- homework/olha_kovanova/Lesson_4/Lesson_4_dict.py
+++ homework/olha_kovanova/Lesson_4/Lesson_4_dict.py
@@ -1,9 +1,8 @@
 my_dict = {'tuple': (1, 2, "text", 4, 5),
-           'list':  [1, 4, "test", "olha", "kovanova"],
-           'dict':  {'one': 'child1', 'two': 'child2', 'three': 'child3',
+           'list': [1, 4, "test", "olha", "kovanova"],
+           'dict': {'one': 'child1', 'two': 'child2', 'three': 'child3',
                     'four': 'child4', 'five': 'child5'},
-            'set':  {1, 3, 7, 3, 9}
-           }
+           'set': {1, 3, 7, 3, 9}}
 
 first_element = my_dict['tuple']
 print(first_element[-1])
