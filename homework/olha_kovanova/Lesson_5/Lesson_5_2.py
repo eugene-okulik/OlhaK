addb346ad5b2527dfb9d136939661562ff740f8d
OlhaKovanova <kovanova.olha@gmail.com>
2024-07-05 14:31:33 +0200

    pep8 changes

--- homework/olha_kovanova/Lesson_5/Lesson_5_2.py
+++ homework/olha_kovanova/Lesson_5/Lesson_5_2.py
@@ -11,9 +11,9 @@ result_of_3st_oper = 'результат работы программы: 9'
 res1 = result_of_1st_oper.index(':')
 res2 = result_of_2st_oper.index(':')
 res3 = result_of_3st_oper.index(':')
-number_1 = result_of_1st_oper[(res1)+2:]
-number_2 = result_of_2st_oper[(res2)+2:]
-number_3 = result_of_3st_oper[(res3)+2:]
+number_1 = result_of_1st_oper[res1 + 2:]
+number_2 = result_of_2st_oper[res2 + 2:]
+number_3 = result_of_3st_oper[res3 + 2:]
 print(int(number_1) + 10)
 print(int(number_2) + 10)
 print(int(number_3) + 10)
