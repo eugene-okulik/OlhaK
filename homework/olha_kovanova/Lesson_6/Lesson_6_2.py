bc56002bfa75f144a036518374c630a1a94dd8cd
OlhaKovanova <kovanova.olha@gmail.com>
2024-07-16 11:51:07 +0200

    Home task lesson 7

--- homework/olha_kovanova/Lesson_6/Lesson_6_2.py
+++ homework/olha_kovanova/Lesson_6/Lesson_6_2.py
@@ -22,11 +22,11 @@
 # Каждое задание выполняйте в отдельном файле
 numbers = list(range(1, 101))
 for num in numbers:
-    if ((num % 3 == 0) and (num % 5 == 0)):
+    if (num % 3 == 0) and (num % 5 == 0):
         print("FuzzBuzz")
-    elif (num % 3 == 0):
+    elif num % 3 == 0:
         print("Fuzz")
-    elif (num % 5 == 0):
+    elif num % 5 == 0:
         print("Buzz")
     else:
         print(num)
