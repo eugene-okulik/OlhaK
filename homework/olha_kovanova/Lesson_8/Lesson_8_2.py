93ac7062c9dc243c3d30b9a7ebe032ba870891e7
OlhaKovanova <kovanova.olha@gmail.com>
2024-08-13 16:42:32 +0200

    Lesson 8 Kovanova Olha

--- homework/olha_kovanova/Lesson_8/Lesson_8_2.py
+++ homework/olha_kovanova/Lesson_8/Lesson_8_2.py
@@ -6,11 +6,11 @@ sys.set_int_max_str_digits(0)
 
 def fib_generator(num):
 
-
     a, b = 0, 1
     for i in range(num):
         yield a
         a, b = b, a + b
+
 
 fib_list = list(fib_generator(100000))
 print(fib_list[4], fib_list[199], fib_list[999], fib_list[99999])
