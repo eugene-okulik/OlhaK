fab94e1b68d4b180060971424f0e1f63a6e41d0f
OlhaKovanova <kovanova.olha@gmail.com>
2024-08-13 16:28:34 +0200

    Lesson 8 Kovanova Olha

---
+++ homework/olha_kovanova/Lesson_8/Lesson_8_1.py
@@ -0,0 +1,22 @@
+# Задание 1
+# Напишите программу. Есть две переменные, salary и bonus. Salary - int, bonus - bool.
+# Спросите у пользователя salary. А bonus пусть назначается рандомом.
+# Если bonus - true, то к salary должен быть добавлен рандомный бонус.
+# Примеры результатов:
+# 10000, True - '$10255'
+# 25000, False - '$25000'
+# 600, True - '$3785'
+import random
+
+salary = int(input("Type your salary: "))
+bonus_list = ['True', 'False']
+bonus = random.choice(bonus_list)
+
+if bonus == 'True':
+    salary_res = salary + random.randint(0, 1000)
+    print(f"{salary}, {bonus} - '${salary_res}'")
+else:
+    print(f"{salary}, {bonus} - '${salary}'")
+
+
+
---
+++ homework/olha_kovanova/Lesson_8/Lesson_8_2.py
@@ -0,0 +1,14 @@
+# Задание 2
+# Напишите функцию-генератор, которая генерирует бесконечную последовательность чисел фибоначчи
+# Распечатайте из этого списка пятое число, двухсотое число, тысячное число, стотысячное число
+import sys
+sys.set_int_max_str_digits(0)
+
+def fib_generator(num):
+    a, b = 0, 1
+    for i in range(num):
+        yield a
+        a, b = b, a + b
+
+fib_list = list(fib_generator(100000))
+print(fib_list[4], fib_list[199], fib_list[999], fib_list[99999])
