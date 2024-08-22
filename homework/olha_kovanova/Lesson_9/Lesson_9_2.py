2a9326fcf5ed244fa5ddf59f052e6a81913f6107
OlhaKovanova <kovanova.olha@gmail.com>
2024-08-22 21:36:07 +0200

    Home task 9 Olha Kovanova

--- homework/olha_kovanova/Lesson_9/Lesson_9_2.py
+++ homework/olha_kovanova/Lesson_9/Lesson_9_2.py
@@ -7,13 +7,15 @@
 # Распечатайте из нового списка самую высокую температуру самую низкую и среднюю.
 import statistics
 
-
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30,
                 32, 30, 28, 24, 23]
 new_list_hot_dates = []
+
+
 def hotest_date(x):
     if x > 28:
         return x
+
 
 new_list_hot_dates = filter(hotest_date, temperatures)
 new_list_hot_dates_f = list(new_list_hot_dates)
@@ -22,4 +24,3 @@ print(max(new_list_hot_dates_f))
 print(min(new_list_hot_dates_f))
 average = statistics.mean(new_list_hot_dates_f)
 print(average)
-
